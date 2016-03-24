f83badaf72bba898cf2e7aa1588ac170bd262732
ophermit <cjhmel@gmail.com>
2016-03-24 14:45:19 +0200

    elevator_saga_00.js - version 1.1
    changed the names of variables and functions

--- elevator_saga_00.js
+++ elevator_saga_00.js
@@ -5,24 +5,22 @@
         queue["down"] = [];
         var LOAD_WARN = 0.8;
 
-        _.each(floors, function(floor) {
+        floors.forEach(function(floor) {
             floor.on("up_button_pressed", function() {
-                register_event(floor.floorNum(), "up");
+                registerEvent(floor.floorNum(), "up");
             });
             floor.on("down_button_pressed", function() {
-                register_event(floor.floorNum(), "down");
+                registerEvent(floor.floorNum(), "down");
             });
         });
 
-        var n_index = 0;
-        _.each(elevators, function(elevator) {
-            elevator.n_num = n_index++;
-            elevator.s_dir = "";
+        elevators.forEach(function(elevator) {
+            elevator.direction = "";
             elevator.on("idle", function() {
-                where_to_go(elevator);
+                whereToGo(elevator);
             });
 
-            elevator.is_stopped = function() {
+            elevator.isStopped = function() {
                 return elevator.destinationDirection() == "stopped" && elevator.destinationQueue.length == 0;
             }
 
@@ -36,90 +34,99 @@
                     goToFloor(elevator, floorNum, true);
                 }
 
-                if (!is_anyvator_here(floorNum, elevator) && elevator.loadFactor() <= LOAD_WARN && queue[direction].indexOf(floorNum) > -1)
+                if (!isAnyvatorHere(floorNum, elevator) && elevator.loadFactor() <= LOAD_WARN && queue[direction].indexOf(floorNum) > -1)
                     goToFloor(elevator, floorNum, true); // make an unscheduled stop to pick up passengers
             });
 
             elevator.on("stopped_at_floor", function(floorNum) {
-                if (floorNum == floors.length - 1) elevator.s_dir = "down"; // if last floor - only "down" direction possible
-                else if (floorNum == 0) elevator.s_dir = "up"; // if first floor - only "up" direction possible
+                if (floorNum == floors.length - 1)
+                    elevator.direction = "down"; // if last floor - only "down" direction possible
+                else if (floorNum == 0)
+                    elevator.direction = "up"; // if first floor - only "up" direction possible
 
-                upd_queue(false, floorNum, elevator.s_dir);
+                updateQueue(false, floorNum, elevator.direction);
             });
         });
 
-        function is_anyvator_here(pn_floor, pn_elevator) {
-            for (var nI = 0; nI < elevators.length; nI++) { // check if any elevator is at the requested floor
-                if (elevators[nI] != pn_elevator && elevators[nI].currentFloor() == pn_floor && elevators[nI].destinationDirection() == 'stopped')
+        function isAnyvatorHere(floorNum, elevator) {
+            var i = -1;
+            while (++i < elevators.length) { // check: if any elevator is at the requested floor
+                if (elevators[i] != elevator && elevators[i].currentFloor() == floorNum && elevators[i].destinationDirection() == 'stopped')
                     return true;
             }
             return false;
         }
 
-        function goToFloor(po_elev, pn_floor, pb_now) {
-            if (po_elev.destinationQueue.length == 0) { // if the queue is empty - set new direction of elevator based on the requested floor
-                if (po_elev.currentFloor() < pn_floor) po_elev.s_dir = "up";
-                else if (po_elev.currentFloor() > pn_floor) po_elev.s_dir = "down";
-            } else if (pb_now === false && po_elev.destinationQueue.indexOf(pn_floor) > -1) // if not immediate move and the elevator's queue already contains this floor - do nothing
+        function goToFloor(elevator, floorNum, immediate) {
+            if (elevator.destinationQueue.length == 0) { // if the queue is empty - set new direction of elevator based on the requested floor
+                if (elevator.currentFloor() < floorNum)
+                    elevator.direction = "up";
+                else if (elevator.currentFloor() > floorNum)
+                    elevator.direction = "down";
+            } else if (immediate === false && elevator.destinationQueue.indexOf(floorNum) > -1) // if not immediate move and the elevator's queue already contains this floor - do nothing
                 return;
 
-            po_elev.goToFloor(pn_floor, pb_now);
+            elevator.goToFloor(floorNum, immediate);
         }
 
-        function upd_queue(pb_add, pn_floor, ps_dir) {
-            if (pb_add === true && queue[ps_dir].indexOf(pn_floor) == -1) // add to floor queue
-                queue[ps_dir].push(pn_floor);
-            else if (pb_add === false && queue[ps_dir].indexOf(pn_floor) > -1) // remove form floor queue
-                queue[ps_dir].splice(queue[ps_dir].indexOf(pn_floor), 1);
+        function updateQueue(addFlag, floorNum, direction) {
+            if (addFlag === true && queue[direction].indexOf(floorNum) == -1) // add to floor queue
+                queue[direction].push(floorNum);
+            else if (addFlag === false && queue[direction].indexOf(floorNum) > -1) // remove form floor queue
+                queue[direction].splice(queue[direction].indexOf(floorNum), 1);
             else
                 return;
         }
 
-        function register_event(pn_floor, ps_dir) { // register a new event of button click on a floor
-            if (!send_to_elevator(pn_floor)) // try to send the event to the nearest free elevator
-                upd_queue(true, pn_floor, ps_dir); // otherwise add the event to the appropriate queue
+        function registerEvent(floorNum, direction) { // register a new event of button click on a floor
+            if (!sendToElevator(floorNum)) // try to send the event to the nearest free elevator
+                updateQueue(true, floorNum, direction); // otherwise add the event to the appropriate queue
         }
 
-        function get_from_down_queue() {
-            var v_t = queue["down"].slice();
-            v_t.sort(function(a, b) {return b - a});
-            return v_t.length > 0 ? v_t[0] : -1;
+        function getFromDownQueue() {
+            var t = queue["down"].slice();
+            t.sort(function(a, b) {return b - a});
+            return t.length > 0 ? t[0] : -1;
         }
 
-        function get_from_up_queue() {
-            var v_t = queue["up"].slice();
-            v_t.sort(function(a, b) {return a - b});
-            return v_t.length > 0 ? v_t[0] : -1;
+        function getFromUpQueue() {
+            var t = queue["up"].slice();
+            t.sort(function(a, b) {return a - b});
+            return t.length > 0 ? t[0] : -1;
         }
 
-        function where_to_go(po_elevator) { // select floor for "idle" elevator
-            var n_where = -1;
-            if (po_elevator.s_dir == "down" && queue["up"].length > 0) n_where = get_from_up_queue();
-            else if (po_elevator.s_dir == "up" && queue["down"].length > 0) n_where = get_from_down_queue();
-            else if (po_elevator.s_dir == "up" && queue["up"].length > 0) n_where = get_from_up_queue();
-            else if (po_elevator.s_dir == "down" && queue["down"].length > 0) n_where = get_from_down_queue();
+        function whereToGo(elevator) { // select floor for "idle" elevator
+            var whereTo = -1;
+            if (elevator.direction == "down" && queue["up"].length > 0)
+                whereTo = getFromUpQueue();
+            else if (elevator.direction == "up" && queue["down"].length > 0)
+                whereTo = getFromDownQueue();
+            else if (elevator.direction == "up" && queue["up"].length > 0)
+                whereTo = getFromUpQueue();
+            else if (elevator.direction == "down" && queue["down"].length > 0)
+                whereTo = getFromDownQueue();
 
-            if (n_where != -1) goToFloor(po_elevator, n_where, false);
+            if (whereTo != -1) goToFloor(elevator, whereTo, false);
         }
 
-        function send_to_elevator(pn_floor) {
-            for (var nI = 0; nI < elevators.length; nI++) { // check: if at least one elevator has this floor in queue - do nothing
-                elev = elevators[nI];
-                if (elev.destinationQueue.length > 0 && elev.destinationQueue.indexOf(pn_floor) > -1 ) return true;
+        function sendToElevator(floorNum) {
+            var i = -1;
+            while (++i < elevators.length) { // check: if at least one elevator has this floor in queue - do nothing
+                if (elevators[i].destinationQueue.length > 0 && elevators[i].destinationQueue.indexOf(floorNum) > -1) return true;
             }
 
             // get min idle elevator
             var selected = null;
-            var n_min = 1000;
-            elevators.forEach(function(po_elevator) {
-                if (po_elevator.is_stopped() && Math.abs(po_elevator.currentFloor() - pn_floor) < n_min) {
-                    n_min = Math.abs(po_elevator.currentFloor() - pn_floor);
-                    selected = po_elevator;
+            var min = 1000;
+            elevators.forEach(function(elevator) {
+                if (elevator.isStopped() && Math.abs(elevator.currentFloor() - floorNum) < min) {
+                    min = Math.abs(elevator.currentFloor() - floorNum);
+                    selected = elevator;
                 }
             });
 
             if (selected != null) { // send the requested floor to the selected elevator
-                goToFloor(selected, pn_floor, false);
+                goToFloor(selected, floorNum, false);
                 return true;
             } else return false; // otherwise - bad try
         }
